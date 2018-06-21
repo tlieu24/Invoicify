3c4c7e374fa0068fbcbb470226cbea5c09cedf88
FZSKPB@ALLYIPOC.COM <FZSKPB@ALLYIPOC.COM>
2018-06-21 09:31:27 -0400

    created the project

---
+++ src/main/java/com/el/ally/wimp/InvoicifyApplication.java
@@ -0,0 +1,12 @@
+package com.el.ally.wimp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoicifyApplication.class, args);
+	}
+}
